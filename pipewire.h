2faa419fa6e5f63f68d6a102da2237f4d8ee4d52
folkert van heusden <mail@vanheusden.com>
2023-06-06 10:32:19 +0200

    clean-up

--- pipewire.h
+++ pipewire.h
@@ -1,4 +1,26 @@
-#include "sound.h"
+#pragma once
+
+#include <thread>
+
+#include <pipewire/pipewire.h>
+#include <spa/param/audio/format-utils.h>
+
+
+class pipewire_data
+{
+public:
+	std::thread       *th            { nullptr };
+
+        pw_main_loop      *loop          { nullptr };
+        pw_stream         *stream        { nullptr };
+	spa_pod_builder    b;
+        const spa_pod     *params[1]     { nullptr };
+        uint8_t            buffer[1024]  { 0       };
+	spa_audio_info_raw saiw          { SPA_AUDIO_FORMAT_UNKNOWN };
+	pw_stream_events   stream_events { 0       };
+};
+
+class sound_parameters;
 
 void configure_pipewire(const int sample_rate, const int bits_per_sample, const int n_channels, sound_parameters *const target);
 void init_pipewire(int *argc, char **argv[]);
--- sound.h
+++ sound.h
@@ -3,27 +3,12 @@
 #include <math.h>
 #include <mutex>
 #include <shared_mutex>
-#include <thread>
 #include <vector>
 
-#include <pipewire/pipewire.h>
-#include <spa/param/audio/format-utils.h>
+#include "pipewire.h"
+
 
 double f_to_delta_t(const double frequency, const int sample_rate);
-
-class pipewire_data
-{
-public:
-	std::thread       *th            { nullptr };
-
-        pw_main_loop      *loop          { nullptr };
-        pw_stream         *stream        { nullptr };
-	spa_pod_builder    b;
-        const spa_pod     *params[1]     { nullptr };
-        uint8_t            buffer[1024]  { 0       };
-	spa_audio_info_raw saiw          { SPA_AUDIO_FORMAT_UNKNOWN };
-	pw_stream_events   stream_events { 0       };
-};
 
 class sound
 {
@@ -35,7 +20,7 @@ private:
 	double delta_t   { 0.   };
 
 public:
-	sound(const int sample_rate, const double frequency, const double amplitude, const std::vector<int> channels) :
+	sound(const int sample_rate, const double frequency, const double amplitude, const std::vector<int> & channels) :
 		frequency(frequency),
 		amplitude(amplitude),
        		channels(channels) {
